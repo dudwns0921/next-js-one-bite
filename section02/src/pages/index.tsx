3e53b2a19d51d7598c6754733f869e59fd29afc7
dudwns0921 <dudwns0921@gmail.com>
2025-02-02 17:22:34 +0900

    fix: ISR 방식 홈 화면에 적용

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -2,32 +2,29 @@ import SearchableLayout from "@/components/SearchableLayout/searchable-layout";
 import { ReactNode } from "react";
 import style from "./index.module.css";
 import BookItem from "@/components/BookItem/book-item";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export const getServerSideProps = async () => {
-  try {
-    const [allBooks, recoBooks] = await Promise.all([
-      fetchBooks(),
-      fetchRandomBooks(),
-    ]);
+export const getStaticProps = async () => {
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
 
-    return {
-      props: {
-        allBooks,
-        recoBooks,
-      },
-    };
-  } catch (error) {
-    console.error(error);
-  }
+  return {
+    props: {
+      allBooks,
+      recoBooks,
+    },
+    revalidate: 3,
+  };
 };
 
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
